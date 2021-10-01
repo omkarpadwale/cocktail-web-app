59ab9337de5867847d49bc8a50e7388ed7eb75f4
Omkar Padwale <omkar.padwale@arcadis.com>
2021-10-02 02:00:22 +0530

    change label

--- src/Component/Header/HeaderSection.js
+++ src/Component/Header/HeaderSection.js
@@ -9,7 +9,7 @@ const buttonStyle = {
 const HeaderSection = (props) => {
     return (
         <Box display="flex" bgcolor="grey.200" p={2} alignItems="center">
-          <Button style= {buttonStyle} color="primary" href='/'> Home </Button>
+          <Button style= {buttonStyle} color="primary" href='/'> Get New Drink </Button>
           {/* <Box >
             <Button style= {buttonStyle} color="secondary" href='/randomdrink'>Random Drink</Button>
           </Box>        */}
